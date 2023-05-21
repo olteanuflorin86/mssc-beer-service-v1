58e66231f9732d7875baef700d02f19aed819015
Florin Olteanu <olteanuflorin86@gmail.com>
2023-05-21 09:35:24 +0300

    First commit

---
+++ src/main/java/com/olteanuflorin86/msscbeerservicev1/MsscBeerServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.olteanuflorin86.msscbeerservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/olteanuflorin86/msscbeerservicev1/MsscBeerServiceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olteanuflorin86.msscbeerservicev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
